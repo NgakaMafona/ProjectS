2bf58a00852ac0551e1bbe785b6d19699b1e495d
Ngaka <drmafona@gmail.com>
2016-11-16 14:29:36 +0200

    Second commit
    1: Added input types
    2: SignUp with username and email address functioning

--- app/src/main/java/layout/SignUp.java
+++ app/src/main/java/layout/SignUp.java
@@ -3,6 +3,7 @@ package layout;
 
 import android.app.Fragment;
 //import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
@@ -14,14 +15,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import za.co.developersjunction.projects.R;
+import za.co.developersjunction.projects.Validations;
+import za.co.developersjunction.projects.loginTest;
+import za.co.developersjunction.projects.pojo.Login;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +42,8 @@ public class SignUp extends Fragment implements View.OnClickListener
     private EditText edt_sign_in_password;
     private TextView tv_sign_up;
 
+    View v;
+
     //TextInputLayout global declaration
     private TextInputLayout til_email;
     private TextInputLayout til_pass;
@@ -41,6 +51,12 @@ public class SignUp extends Fragment implements View.OnClickListener
     //Button global declaration
     private Button btn_google_signUp;
     private Button btn_on_signUp;
+
+    //check for valid email and password
+    private boolean valid_email;
+    private boolean valid_password;
+
+    private boolean isSucc;
 
     //Firebase global declaration
     private GoogleApiClient mGoogleApiClient;
@@ -58,7 +74,7 @@ public class SignUp extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        v = inflater.inflate(R.layout.fragment_sign_up, container, false);
 
         //Initialise EditTexts
         edt_sign_in_email = (EditText) v.findViewById(R.id.edt_sign_in_email);
@@ -122,7 +138,66 @@ public class SignUp extends Fragment implements View.OnClickListener
         }
         else if(btn_id ==  R.id.btn_email_sign_up)
         {
+            Validations val = new Validations();
+            Login l;
 
+            //get text from edittexts
+            String email = edt_sign_in_email.getText().toString();
+            String password = edt_sign_in_password.getText().toString();
+
+            //validate email and password
+            valid_email =  val.isValidEmail(email);
+            valid_password = val.validPassword(password);
+
+            //check for empty string
+            if(email.equals(""))
+            {
+                til_email.setError("Enter an email address");
+            }
+            else
+            {
+                til_email.setErrorEnabled(false);
+
+                if(!valid_email)
+                {
+                    til_email.setError("Enter a valid email address");
+                }
+                else
+                {
+                    til_email.setError("");
+                }
+            }
+
+            //check for password empty string
+            if(password.equals(""))
+            {
+                til_pass.setError("Enter a password");
+            }
+            else
+            {
+                til_pass.setError("");
+
+                if(!valid_password)
+                {
+                    til_pass.setError("Enter a valid password");
+                }
+                else
+                {
+                    til_pass.setError("");
+                }
+            }
+
+            if(valid_email && valid_password)
+            {
+                l = new Login(email,password);
+                createAccount(l);
+
+                if (isSucc)
+                {
+                    startActivity(new Intent(getActivity(),loginTest.class));
+                }
+
+            }
         }
 
     }
@@ -140,5 +215,29 @@ public class SignUp extends Fragment implements View.OnClickListener
     {
         super.onStop();
         mGoogleApiClient.disconnect();
+    }
+
+    public void createAccount(Login l)
+    {
+        mFirebaseAuth.createUserWithEmailAndPassword(l.getEmail(),l.getPassword())
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(!task.isSuccessful())
+                        {
+                            isSucc = false;
+
+                            Toast.makeText(getActivity(),"Error creating account " + task.isSuccessful(),Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            isSucc = true;
+
+                            Toast.makeText(getActivity(),"Account Created Successfully" + task.isSuccessful(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/za/co/developersjunction/projects/loginTest.java
@@ -0,0 +1,15 @@
+package za.co.developersjunction.projects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginTest extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_test);
+    }
+}
